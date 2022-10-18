869f036bff59e82941b055446a6830cee509f09d
Bismar Trujillo <bismar20@hotmail.com>
2022-10-18 10:08:01 -0400

    rule of 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,8 @@
+//rule of 72
+
+var interestRate = 14.00;
+var years = 72 / interestRate;
+
+console.log("At a " + interestRate.toFixed(2) + 
+            " interest rate, your  savings account will be worth " + years.toFixed(2) + " in some years");
+
