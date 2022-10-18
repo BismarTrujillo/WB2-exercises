74fd82f4e2bcdedfbed4fb0ed832bc21a226ad13
Bismar Trujillo <bismar20@hotmail.com>
2022-10-18 09:54:26 -0400

    tip amount

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,7 @@
+//tip amount given percentage
+
+var foodBill = 130.32;
+var percentage = 20;
+var tipAmount = foodBill * percentage / 100;
+
+console.log("The tip on a " + foodBill + " food bill is " + tipAmount.toFixed(2));
