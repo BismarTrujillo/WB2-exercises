ff6cf61314c317491879e2e9064a883917a6f49a
Bismar Trujillo <bismar20@hotmail.com>
2022-10-19 13:50:21 -0400

    gregorian calender exercise

---
+++ IfScripts/gregorian_calendar.js
@@ -0,0 +1,13 @@
+var year = 2016;
+
+if (year % 4 != 0) {
+    console.log(year + " is not a leap year");
+} else if (year % 100 != 0) {
+    console.log(year + " is a leap year");
+} else if (year % 400 == 0) {
+    console.log(year + " is a leap year");
+} else {
+    console.log(year + " is not a leap year")
+}
+
+
--- IfScripts/pay_rules.js
+++ IfScripts/pay_rules.js
@@ -1,11 +1,12 @@
+
 var payRate = 17.30;
 var hoursWorked = 45;
-
+// first check if worked over 40 hours
 if (hoursWorked <= 40) {
     grossPay = payRate * hoursWorked;
 }
 else {
-    overtime = (hoursWorked - 40) * (1.5 * payRate);
+    overtime = (hoursWorked - 40) * (1.5 * payRate); //first calculate the overtime pay
     grossPay = payRate * 40 + overtime;
 }
 
