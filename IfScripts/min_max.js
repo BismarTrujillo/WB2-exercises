ce653af45c6ac57f5eb8589ec01338442f487cec
Bismar Trujillo <bismar20@hotmail.com>
2022-10-19 16:01:03 -0400

    min_max

---
+++ IfScripts/min_max.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = 39;
+var c = 3;
+
+if (a < b && a < c) {
+    console.log(a + " is the smallest number");
+} else if (b < a && b < c){
+    console.log(b + " is the smallest number");
+} else {
+    console.log(c + " is the smallest number");
+}
+if (a > b && a > c) {
+    console.log(a + " is the largest number");
+} else if (b > a && b > c){
+    console.log(b + " is the largest number");
+} else {
+    console.log(c + " is the largest number");
+}
