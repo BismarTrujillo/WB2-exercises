880ec5ef7ddc33736270c0c8147bd85b5ec40726
Bismar Trujillo <bismar20@hotmail.com>
2022-10-19 14:10:18 -0400

    greetings exercise

---
+++ IfScripts/greeting.js
@@ -0,0 +1,10 @@
+var hour = 0;
+
+if (hour < 10 && hour > 0) {
+    console.log("Good morning!");
+} else if (hour < 17 && hour > 10) {
+    console.log("Good day!");
+
+} else {
+    console.log("Good evening;")
+}
