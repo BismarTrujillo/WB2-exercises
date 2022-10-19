3608892111f063e7669b1c772ca212c189a32fc6
Bismar Trujillo <bismar20@hotmail.com>
2022-10-19 16:15:04 -0400

    show_major

---
+++ IfScripts/show_major.js
@@ -0,0 +1,21 @@
+var studentName = "Monica";
+var studentMajor = "HIST";
+
+if (studentMajor == "BIOL") {
+    console.log("Major: " + studentMajor);
+    console.log("Advising Location: Science Bldg, Room 310");
+} else if (studentMajor == "CSCI") {
+    console.log("Major: " + studentMajor);
+    console.log("Sheppard Hall, Room 314");
+} else if (studentMajor == " ENG") {
+    console.log("Major: " + studentMajor);
+    console.log("Kerr Hall, Room 201");
+} else if (studentMajor == "HIST") {
+    console.log("Major: " + studentMajor);
+    console.log("Kerr Hall, Room 114");
+} else if (studentMajor == "MKT") {
+    console.log("Major: " + studentMajor);
+    console.log("Westly Hall, Room 310");
+}else {
+    console.log("<unknown>");
+}
